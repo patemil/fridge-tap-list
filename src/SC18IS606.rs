bbddbf8858d8928aa50d46bd5e6a81ddd5a49e1c
Michael Unneback <michael@folkdatorn.se>
2023-02-06 17:42:52 +0100

    Update SC18IS606.rs

--- src/SC18IS606.rs
+++ src/SC18IS606.rs
@@ -1,17 +1,18 @@
 use embedded_hal::blocking::i2c::WriteRead;
 
-const LM75_I2CADDR: u8 = 0x48;
+const SC18IS606_I2CADDR: u8 = 0x50;
 
-pub struct LM75<I> {
+pub struct SC18IS606<I> {
     device: I,
 }
 
-impl<I: WriteRead> LM75<I> {
+impl<I: WriteRead> SC18IS606<I> {
     /// Create device driver instance.
     pub fn new(i2c: I) -> Self {
-        LM75 { device: i2c }
+        SC18IS606 { device: i2c }
     }
 
+    
     pub fn measure(&mut self) -> Result<f32, I::Error> {
         let raw = self.read_u16(0x00)?;
         Ok((raw >> 8) as f32 + (0.5 * ((raw >> 7) & 0b1) as f32))
